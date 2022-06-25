4ac48efbd9cb5bcfec2bbc24bfb577795e2b0139
aevalenc <aevalenc@syr.edu>
2022-06-25 17:08:00 +0200

    Created Numerical Methods repo

---
+++ Backwards-Substitution/src/backwards-substitution.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: Alejandro Valencia
+ * Project: Backwards Substitution Matrix Solver
+*/
+
+namespace nm
+{
+  namespace matrix
+  {
+    void BackwardsSubstitution();
+    void RowEchelon();
+    void LolFunction()
+    {
+      RowEchelon();
+    }
+  }
+}
