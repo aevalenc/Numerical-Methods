cae97c3af28542a2f057790a8f03bce332dcdab4
aevalenc <aevalenc@syr.edu>
2022-06-25 17:25:38 +0200

    Cleaning up auto-save files

---
+++ Newtons-Method/src/functions.h
@@ -0,0 +1,14 @@
+/*
+ * Author : Alejandro Valencia
+ * Project: Numerical-Methods
+ * Update : 11 June, 2022
+*/
+
+
+double f(double x){
+  return (x * (1-x));  
+}
+
+double fprime(double x){
+  return (-2*x + 1);
+}
---
+++ Newtons-Method/src/newtons_method.h
@@ -0,0 +1,11 @@
+/*
+ * Author : Alejandro Valencia
+ * Project: Newton's Method
+ * Update : 11 June, 2022 
+*/
+
+#include <limits>
+#include <iostream>
+#include "functions.h"
+
+double NewtonsMethod(double& x_0);
