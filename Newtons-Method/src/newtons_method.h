dad1e2a329ac42b988c586dc84af48d7c526eaa8
aevalenc <aevalenc@syr.edu>
2022-06-26 12:34:16 +0200

    Include hedronvision bazel compile commands repo
    This allows for clangd lsp auto compeletion

--- Newtons-Method/src/newtons_method.h
+++ Newtons-Method/src/newtons_method.h
@@ -1,16 +1,13 @@
 /*
  * Author : Alejandro Valencia
  * Project: Newton's Method
- * Update : 11 June, 2022 
-*/
+ * Update : 11 June, 2022
+ */
 
-#include <limits>
-#include <iostream>
 #include "functions.h"
-namespace nm
-{
-  namespace polysolver
-  {
-    double NewtonsMethod(double &x_0);
-  }
+#include <limits>
+namespace nm {
+namespace polysolver {
+double NewtonsMethod(double &x_0);
 }
+} // namespace nm
