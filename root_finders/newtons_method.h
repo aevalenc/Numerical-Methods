3f1cd5960ca4c9e855ade262f17c9db53650e144
aevalenc <aevalenc@syr.edu>
2023-03-12 13:20:26 +0100

    Refactor repo

--- root_finders/newtons_method.h
+++ root_finders/newtons_method.h
@@ -1,13 +1,20 @@
 /*
  * Author : Alejandro Valencia
  * Project: Newton's Method
- * Update : 11 June, 2022
+ * Update : 12 March, 2023
  */
 
-#include "functions.h"
+#include "root_finders/functions.h"
 #include <limits>
-namespace nm {
-namespace polysolver {
+
+namespace nm
+{
+
+namespace polysolver
+{
+
 double NewtonsMethod(double &x_0);
+
 }
+
 } // namespace nm
