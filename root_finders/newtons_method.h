4cae6696d52ea37e810913386c68f198d1bdc685
Alejandro Valencia <aevalenc@syr.edu>
2023-09-10 12:30:17 +0200

    Fix formatting to newtons method
    In addition, remove functions.h and have netwons method function take functional arguments

--- root_finders/newtons_method.h
+++ root_finders/newtons_method.h
@@ -4,7 +4,7 @@
  * Update : 12 March, 2023
  */
 
-#include "root_finders/functions.h"
+#include <functional>
 #include <limits>
 
 namespace nm
@@ -13,8 +13,12 @@ namespace nm
 namespace polysolver
 {
 
-double NewtonsMethod(double &x_0);
+double NewtonsMethod(double& x_0,
+                     const std::function<double(double)>& function,
+                     const std::function<double(double)>& derivative,
+                     const double tolerance = 0.001,
+                     const std::int32_t max_iterations = 1000);
 
 }
 
-} // namespace nm
+}  // namespace nm
