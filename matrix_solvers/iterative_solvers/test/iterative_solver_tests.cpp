01226b4baa98d1015765eb58f3f35618bf2ee495
Alejandro Valencia <aevalenc@syr.edu>
2024-04-13 22:32:03 +0200

    Add unit tests for single iteration conjugate gradient

--- matrix_solvers/iterative_solvers/test/iterative_solver_tests.cpp
+++ matrix_solvers/iterative_solvers/test/iterative_solver_tests.cpp
@@ -3,8 +3,10 @@
  * Project: Jacobi - main unit tests
  */
 
+#include "matrix_solvers/iterative_solvers/conjugate_gradient.h"
 #include "matrix_solvers/iterative_solvers/gauss_seidel.h"
 #include "matrix_solvers/iterative_solvers/jacobi.h"
+#include "matrix_solvers/utilities.h"
 #include <gtest/gtest.h>
 
 namespace nm
@@ -16,7 +18,7 @@ namespace matrix
 namespace
 {
 
-class JacobiMethodTestFixture : public ::testing::Test
+class IterativeSolversBaseTestFixture : public ::testing::Test
 {
     void SetUp() override
     {
@@ -30,14 +32,14 @@ class JacobiMethodTestFixture : public ::testing::Test
     }
 
   public:
+    Matrix<double> A{};
     std::vector<double> b{};
-    std::vector<std::vector<double>> A{};
     std::vector<double> x{0.0, 0.0, 0.0};
     double tolerance_{0.001};
     double max_iterations_{1000};
 };
 
-class GaussSeidelTestFixture : public JacobiMethodTestFixture
+class JacobiMethodTestFixture : public IterativeSolversBaseTestFixture
 {
 };
 
@@ -70,6 +72,10 @@ TEST_F(JacobiMethodTestFixture, GivenValidMatrix_ExpectSingleIterationToMatchWit
     EXPECT_NEAR(x.at(0), x_void.at(0), tolerance_);
 }
 
+class GaussSeidelTestFixture : public IterativeSolversBaseTestFixture
+{
+};
+
 TEST_F(GaussSeidelTestFixture, GivenValidMatrix_ExpectConvergedSolution)
 {
     double residual{0.0};
@@ -97,6 +103,53 @@ TEST_F(GaussSeidelTestFixture, GivenValidMatrix_ExpectSingleIterationToMatchWith
 
     GaussSeidel(A, b, x_void, max_iterations_, tolerance_);
     EXPECT_NEAR(x.at(0), x_void.at(0), tolerance_);
+}
+
+class ConjugateGradientTestFixture : public IterativeSolversBaseTestFixture
+{
+  public:
+    void SetUp() override
+    {
+        A_.push_back(std::vector<double>{4.0, 1.0});
+        A_.push_back(std::vector<double>{1.0, 3.0});
+
+        b_.push_back(1);
+        b_.push_back(2);
+
+        x.resize(2);
+
+        max_iterations_ = 10;
+    }
+
+  public:
+    Matrix<double> A_{};
+    std::vector<double> b_{};
+    std::vector<double> x_expected_{0.0909, 0.6364};
+};
+
+TEST_F(ConjugateGradientTestFixture, GivenValidMatrix_ExpectConvergedSolution)
+{
+    // Given
+    const auto n = static_cast<std::int32_t>(b_.size());
+    auto residual = CalculateResidual(A_, b_, x, n);
+    auto p(residual);
+
+    std::pair<std::vector<double>, std::vector<double>> result{};
+    for (std::size_t iteration = 0; iteration < max_iterations_; ++iteration)
+    {
+        if (L2Norm(residual) < tolerance_)
+        {
+            break;
+        }
+        result = ConjugateGradient(A_, b_, x, residual, p);
+        residual = result.first;
+        p = result.second;
+    }
+
+    for (std::int32_t i{0}; i < n; ++i)
+    {
+        EXPECT_NEAR(x.at(i), x_expected_.at(i), tolerance_);
+    }
 }
 
 }  // namespace
