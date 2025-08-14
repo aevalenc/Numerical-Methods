ee830f3871e91a03b28e124d289eca7f4803ab77
aevalenc <valencia.alex70@gmail.com>
2025-08-14 15:46:18 +0200

    Add more tests

--- root_finders/test/bisection_method_tests.cpp
+++ root_finders/test/bisection_method_tests.cpp
@@ -4,8 +4,8 @@
  * Update: September 9, 2023
  */
 
-#include "gtest/gtest.h"
 #include "root_finders/bisection_method/bisection_method.h"
+#include <cmath>
 #include <gtest/gtest.h>
 
 namespace nm
@@ -23,6 +23,7 @@ struct BisectionMethodTestParameter
     double a;
     double b;
     double expected_value;
+    double algorithm_tolerance{1e-6};
 };
 
 class BisectionMethodTestFixture : public ::testing::TestWithParam<BisectionMethodTestParameter>
@@ -38,12 +39,22 @@ TEST_P(BisectionMethodTestFixture, GivenValidInputs_ExpectValidOutput)
     EXPECT_NEAR(result, param.expected_value, expectation_tolerance_);
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     BisectionMethodTests,
     BisectionMethodTestFixture,
     ::testing::Values(
         BisectionMethodTestParameter{[](const double x) -> double { return (x * (1.0 - x)); }, -2.0, 0.5, 0.0},
-        BisectionMethodTestParameter{[](const double x) -> double { return (x * (1.0 - x)); }, 0.5, 1.5, 1.0}));
+        BisectionMethodTestParameter{[](const double x) -> double { return (x * (1.0 - x)); }, 0.5, 1.5, 1.0},
+        BisectionMethodTestParameter{[](const double x) -> double { return ((x * x * x) - 30 * (x * x) + 2552); },
+                                     0.0,
+                                     20.0,
+                                     11.861501,
+                                     1e-8},
+        BisectionMethodTestParameter{[](const double x) -> double { return 2.5 * sinh(x / 4) - 1; },
+                                     -10.0,
+                                     10.0,
+                                     1.5601412,
+                                     1e-6}));
 
 }  // namespace
 
