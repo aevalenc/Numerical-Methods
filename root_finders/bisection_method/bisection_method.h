688a1f738f840dcdae176e19251bd331e610a127
aevalenc <valencia.alex70@gmail.com>
2025-08-14 11:48:10 +0200

    First bite at bisection method

---
+++ root_finders/bisection_method/bisection_method.h
@@ -0,0 +1,27 @@
+/*
+ * Bisection method for finding roots of a continuous function.
+ *
+ * Author: Alejandro Valencia
+ * Update: August 14, 2025
+ */
+
+#ifndef ROOT_FINDERS_BISECTION_METHOD_BIDSECTION_METHOD_H
+#define ROOT_FINDERS_BISECTION_METHOD_BIDSECTION_METHOD_H
+
+#include <functional>
+
+namespace nm
+{
+namespace root_finders
+{
+
+double BisectionMethod(const std::function<double(double)>& function,
+                       const double a,
+                       const double b,
+                       const double tolerance);
+
+}  // namespace root_finders
+
+}  // namespace nm
+
+#endif  // ROOT_FINDERS_BISECTION_METHOD_BISECTION_METHOD_H
