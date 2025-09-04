5c0cc6f7fce5adbf1fc7f4a41d33cb9ac0d9b4fd
aevalenc <valencia.alex70@gmail.com>
2025-09-04 10:45:34 +0200

    Some clean up on Newton-Kleinman method

--- controls/lqr/newton_kleinman.cpp
+++ controls/lqr/newton_kleinman.cpp
@@ -20,44 +20,44 @@ std::pair<matrix::Matrix<double>, matrix::Matrix<double>> NewtonKleinman(const m
                                                                          const matrix::Matrix<double>& B,
                                                                          const matrix::Matrix<double>& Q,
                                                                          const matrix::Matrix<double>& R,
-                                                                         const matrix::Matrix<double>& K0)
+                                                                         const matrix::Matrix<double>& K0,
+                                                                         const std::int32_t max_iterations,
+                                                                         const double tolerance)
 {
     // Apply Kronecker product
-    // To solve the system S0'P + PS0 = -Q - K0'RK0
-    const std::int32_t max_iterations = 1000;
-    const double tolerance = 1e-12;
-
+    // To solve the system S0'P + PS0 = -Q - K'RK
     const auto I = matrix::CreateIdentityMatrix<double>(A.size());
-    matrix::Matrix<double> P(Q.size(), Q.size());
+    const auto m = static_cast<std::int32_t>(Q.size());
+    matrix::Matrix<double> P{m, m};
     auto K_previous = K0;
     auto K_next = K0;
+
+    // Calculate inverse input cost matrix R
     matrix::Matrix<double> R_inv{R};
-    R_inv.at(0).at(0) = 1 / R.at(0).at(0);
+    const auto n = static_cast<std::int32_t>(R.size());
+    for (std::int32_t i{0}; i < n; ++i)
+    {
+        R_inv.at(i).at(i) = 1 / R.at(i).at(i);
+    }
 
     for (std::int32_t iter{0}; iter < max_iterations; ++iter)
     {
-        const auto BK = matrix::MatMult(B, K_previous);
-        auto S = A + BK;
+        auto S = A - matrix::MatMult(B, K_previous);
 
-        const auto K_transposed = K_previous.Transpose();
-        const auto KTRK = matrix::MatMult((MatMult(K_transposed, R)), K_previous);
-        const auto RHS = Q + KTRK;
+        const auto KT_RK = matrix::MatMult((MatMult(K_previous.Transpose(), R)), K_previous);
+        const auto RHS = Q + KT_RK;
         const auto negative_RHS = matrix::ScalarMultiply(-1.0, RHS);
-
         const auto RHS_vectorized = matrix::Vectorize(negative_RHS);
-        S.TransposeInPlace();
-        const auto IS = matrix::KroneckerProduct(I, S);
-        const auto SI = matrix::KroneckerProduct(S, I);
 
-        const auto AA = (IS + SI);
+        S.TransposeInPlace();
+        const auto AA = matrix::KroneckerProduct(I, S) + matrix::KroneckerProduct(S, I);
         const auto P_vectorized = matrix::LUSolve(AA, RHS_vectorized);
 
         P = matrix::Devectorize(P_vectorized, negative_RHS.size());
         const auto RinvBT = matrix::MatMult(R_inv, B.Transpose());
-        K_next = matrix::ScalarMultiply(-1.0, matrix::MatMult(RinvBT, P));
+        K_next = matrix::MatMult(RinvBT, P);
 
-        const auto negative_K0 = matrix::ScalarMultiply(-1.0, K_previous);
-        const auto delta_K = K_next + negative_K0;
+        const auto delta_K = K_next - K_previous;
         const auto residual = matrix::L2Norm(matrix::Vectorize(delta_K));
         if (residual < tolerance)
         {
