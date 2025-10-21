5e61816ebcabf07264980961400e6c5d785eeb96
Alejandro Valencia <valencia.alex70@gmail.com>
2025-10-21 12:20:56 +0200

    Refine secant implementation

--- root_finders/secant_method/multivar_secant_method.cpp
+++ root_finders/secant_method/multivar_secant_method.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "root_finders/secant_method/multivar_secant_method.h"
+#include "matrix_solvers/direct_solvers/lu_solve.h"
 #include "matrix_solvers/operations/operations.h"
 #include "matrix_solvers/utilities.h"
 #include <cstdint>
@@ -36,7 +37,8 @@ matrix::Matrix<double> EvaluateSystem(const std::vector<std::function<double(std
         i++;
         j = 0;
     }
-    return A.Transpose();
+    const auto A_transposed = A.Transpose();
+    return A_transposed;
 }
 }  // namespace
 
@@ -47,27 +49,43 @@ std::vector<double> MultiVarSecantMethod(const std::vector<std::function<double(
                                          const double tolerance,
                                          const std::int32_t max_iterations)
 {
-    std::vector<double> xkm1{x0};
-    std::vector<double> xk{x1};
-    std::vector<double> xkp1{0.0};
-    std::vector<std::vector<double>> arguments = {xkm1, xk, xkp1};
+    std::vector<double> xkm2{x0};
+    std::vector<double> xkm1{x1};
+    std::vector<double> xk{x2};
+    std::vector<double> xkp1{};
+    xkp1.resize(x0.size());
+
+    std::vector<double> b{};
+    b.resize(x0.size() + 1);
+    b.at(0) = 1;
+
     for (std::int32_t k{1}; k < max_iterations; ++k)
     {
+        std::vector<std::vector<double>> arguments = {xkm2, xkm1, xk};
         const auto A = EvaluateSystem(equations, arguments);
+        const auto coefficients = matrix::LUSolve(A, b);
 
-        //     if (matrix::L2Norm(matrix::AddVectors(xkp1, matrix::ScalarMultiply(-1.0, xk))) < tolerance)
-        //     {
-        //         std::cout << "Secant method converged in " << k << " iterations.\n";
-        //         break;
-        //     }
-        //     if (k == max_iterations - 1)
-        //     {
-        //         std::cout << "Max iterations reached.\n";
-        //     }
-        //     xkm1 = xk;
-        //     xk = xkp1;
+        for (std::int32_t i{0}; i < static_cast<std::int32_t>(arguments.size()); ++i)
+        {
+            const auto c = matrix::ScalarMultiply(coefficients.at(i), arguments.at(i));
+            xkp1 = matrix::AddVectors(xkp1, c);
+        }
+
+        if (matrix::L2Norm(matrix::AddVectors(xkp1, matrix::ScalarMultiply(-1.0, xk))) < tolerance)
+        {
+            std::cout << "Secant method converged in " << k << " iterations.\n";
+            break;
+        }
+        if (k == max_iterations - 1)
+        {
+            std::cout << "Max iterations reached.\n";
+        }
+        xkm2 = xkm1;
+        xkm1 = xk;
+        xk = xkp1;
+        xkp1.assign(xkp1.size(), 0);
     }
-    return xkp1;
+    return xk;
 }
 
 }  // namespace root_finders
