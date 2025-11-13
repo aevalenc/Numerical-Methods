0050fa099d0fe3a017d79e476892469056f26048
Alejandro Valencia <valencia.alex70@gmail.com>
2025-11-13 09:32:07 +0100

    Uniform distribution

--- monte_carlo/vacation_budget.py
+++ monte_carlo/vacation_budget.py
@@ -12,13 +12,14 @@ def main():
 
     rng_generator = np.random.default_rng(seed=10)
 
-    number_of_samples = 10
+    number_of_samples = 1000
 
-    flight_cost_min = 1
-    flight_cost_max = 6
+    flight_cost_min = 500
+    flight_cost_max = 1000
     # flight_cost_dist = np.random.uniform(flight_cost_min, flight_cost_max, number_of_samples)
-    flight_cost_samples = rng_generator.integers(flight_cost_min, flight_cost_max, number_of_samples, endpoint=True)
-    print(f"Flight cost samples: {flight_cost_samples}")
+    # flight_cost_samples = rng_generator.uniform(flight_cost_min, flight_cost_max, number_of_samples, endpoint=True)
+    flight_cost_samples = rng_generator.uniform(flight_cost_min, flight_cost_max, number_of_samples)
+    # print(f"Flight cost samples: {flight_cost_samples}")
 
     hotel_nightly_cost_min = 80
     hotel_nightly_cost_max = 200
@@ -33,20 +34,21 @@ def main():
     # plt.plot(bins, np.ones_like(bins), linewidth=2, color="r")
 
     plt.figure()
-    counts, bins = np.histogram(flight_cost_samples, bins=6, density=False)
+    counts, bins = np.histogram(flight_cost_samples, bins=500, density=True)
     print(f"Bins: {bins}")
     print(f"Counts: {counts}")
-    # plt.bar(bins[:-1], counts, edgecolor="black")
-    plt.hist(flight_cost_samples, bins="auto", edgecolor="black")
+    plt.plot(bins[:-1], counts)
+    # plt.hist(flight_cost_samples, edgecolor="black", align="mid")
     plt.title("Flight Cost Distribution")
 
-    # plt.figure()
-    # plt.hist(flight_cost_samples, 6, density=True)
-    # plt.title("Flight Cost Distribution Density")
-    # plt.hist(hotel_nightly_cost_samples, number_of_samples, density=True)
+    plt.figure()
+    # plt.hist(bins[:-1], bins, weights=counts)
+    plt.title("Flight Cost Distribution Density")
+    plt.hist(flight_cost_samples, 500, density=True)
 
-    # plt.figure()
-    # plt.hist(total_cost_dist, number_of_samples, cumulative=True, density=True)
+    plt.figure()
+    plt.title("Flight Cost Distribution Cummulative")
+    plt.hist(total_cost_dist, number_of_samples, cumulative=True, density=True)
     plt.show()
 
     print(np.mean(total_cost_dist))
